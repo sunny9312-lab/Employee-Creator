071ff4927523d4817ed4923ec8ff065e823736dc
sunny9312-lab <daisey9312@gmail.com>
2023-09-12 20:13:41 +1000

    js types done

---
+++ js/types/script.js
@@ -0,0 +1,43 @@
+console.log("her i am");
+
+console.warn("be careful");
+console.log("number2");
+console.error("something went wrong");
+console.log(typeof "hi");
+console.log(typeof 123);
+("hello");
+console.log(10 > 5);
+console.log(5 < 6);
+//undefined
+
+let i;
+console.log(i);
+//null
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(10 + 2);
+console.log(2 * 8);
+console.log("hello" + " nology");
+console.log("helloooo" - "o");
+console.log("1" + 1);
+console.log(parseInt("1"));
+// challenge
+console.log("1", typeof 15); //number
+console.log("2", typeof 5.5); //number
+console.log("2-1", typeof "5.5"); //number
+console.log("3", typeof NaN); //number
+console.log("4", typeof "hello"); //string
+console.log("5", typeof true); //boolean
+console.log("6", typeof 1 != 2); //true
+console.log("7", typeof ("hamburgers" + "s")); //string
+console.log("8", typeof ("hamburgers" - "s")); //number
+console.log("8-1", "hamburgers" - "s"); //NaN
+console.log("9", 2 === "2"); //false
+console.log("10", typeof ("1" + "5")); //string
+console.log("11", "1" - "5"); //number -4
+console.log("11-1", typeof ("1" - "5")); //number
+console.log("12", typeof ("Johnny" + 5));
+console.log("13", "Johnny" - 5); //NaN
+console.log("13-1", typeof ("Johnny" - 5)); //number number
+console.log("14", 99 * "luftballons"); //NaN
+console.log("14-1", typeof (99 * "luftballons")); //number
